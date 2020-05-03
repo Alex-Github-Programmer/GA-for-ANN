35f02dce25a2e632d17e7f4cb0ff970b9a8fd0d5
Alex-Github-Programmer <55904367+Alex-Github-Programmer@users.noreply.github.com>
2020-05-03 14:19:19 +0800

    Add files via upload

--- 0.0.0debugged 2020y124d/__main__.py
+++ 0.0.0debugged 2020y124d/__main__.py
@@ -1,1 +1,29 @@
-
+#__main__.py
+import network
+import population
+import selection
+import crossover
+import mutation
+import math
+import random
+class Population(population.Population):
+    select = selection.stochastic_tournament
+    cross = crossover.uniform_crossover
+    mutate = mutation.simple_mutation
+    def fitness(self):
+        f = []
+        for i in self.chromosome:
+            a = 0
+            for j in i:
+                a = (a << 1) | j
+            a /= (1 << 16)
+            f.append(a * math.sin(10 * math.pi * a) + 10)
+        return f
+ch = []
+for i in range(100):
+    ch.append([bool(random.randint(0, 1)) for i in range(19)])
+p = Population(ch, 100, {}.fromkeys(range(19), (0.01, 1, 1000)))
+print(max(p.fitness()))
+for i in range(1000):
+    p.evolute()
+    print(max(p.fitness()))
---
+++ 0.0.0debugged 2020y124d/mutation.py
@@ -0,0 +1,52 @@
+from random import random as random_random, \
+     randint as random_randint, \
+     gauss as random_gauss, \
+     uniform as random_uniform, \
+     choice as random_choice
+def simple_mutation(self):
+    for i in self.chromosome:
+        for j in self.mutation_dict.items():
+            value = random_random()
+            if value < j[1][0]:
+                i[j[0]] = not i[j[0]]
+def uniform_mutation(self):
+    for i in self.chromosome:
+        for j in self.mutation_dict.items():
+            value = random_random()
+            if value < j[1][0]:
+                i[j[0]] = random_uniform(j[1][1], j[1][2])
+def boundary_mutation(self):
+    for i in self.chromosome:
+        for j in self.mutation_dict.items():
+            value = random_random()
+            if value < j[1][0]:
+                i[j[0]] = random_choice([j[1][1], j[1][2]])
+def ununiform_mutation(self):
+    for i in self.chromosome:
+        for j in self.mutation_dict.items():
+            value = random_random()
+            if value < j[1][0]:
+                i[j[0]] = random_uniform(
+                    j[1][1] + i[j[0]],
+                    j[1][2] + i[j[0]])
+def gauss_mutation(self, p = 1):
+    for i in self.chromosome:
+        for j in self.mutation_dict.items():
+            value = random_random()
+            if value < j[1][0]:
+                i[j[0]] += random_gauss(0, p)
+if __name__ == '__main__':
+    import population
+    class Test(population.Population):
+        fitness = lambda self:self.chromosome
+    funclist = [simple_mutation,
+                uniform_mutation,
+                boundary_mutation,
+                ununiform_mutation,
+                gauss_mutation]
+    for i in funclist:
+        print(i.__name__)
+        Test.mutate = i
+        a = Test([[i] for i in range(1000)], 1000, {0:(0.1, 1, 1000)})
+        a.mutate()
+        print(a.chromosome)
